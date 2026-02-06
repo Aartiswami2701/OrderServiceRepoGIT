1c7edc2cf88b00b506b8cc0ebb178bc74fbb248d
Aarti Swami <aarti.swami@capgemini.com>
2026-02-06 14:36:02 +0530

    Revert "feat: updated getmapping path"
    This reverts commit cd9af9c8af850e78abf383807983f517cba73e74.

--- src/main/java/com/example/orders/controller/OrdersController.java
+++ src/main/java/com/example/orders/controller/OrdersController.java
@@ -11,12 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/orders")
 public class OrdersController {
 	
-	@PostMapping("/createorder")
+	@PostMapping("/create")
 	public String createOrders() {
 		return "order created";
 	}
 	
-	@GetMapping("/findorderbyid/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<String> getOrder(@PathVariable String id) {
 		if(id==null ||id.trim().isEmpty()) {
 			return ResponseEntity.notFound().build();
