847379db386fd2b9b06e3b6a824f44b4b417d5b1
Aarti Swami <aarti.swami@capgemini.com>
2026-02-06 11:45:53 +0530

    feat(controller): create order basic handler

--- src/main/java/com/example/orders/controller/OrdersController.java
+++ src/main/java/com/example/orders/controller/OrdersController.java
@@ -1,15 +1,18 @@
 package com.example.orders.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.orders.entity.Order;
 
 @RestController
 @RequestMapping("/orders")
 public class OrdersController {
 	
 	@PostMapping("/create")
-	public String createOrders() {
+	public String createOrders(@RequestBody Order order) {
 		return "order created";
 	}
 }
